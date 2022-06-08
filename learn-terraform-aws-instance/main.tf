a6801d5dd8472f9f518a61f144724867106aa885
Martin Perez Rodriguez <mperezrodriguez95@gmail.com>
2022-06-08 02:33:23 +0200

    Initial commit

---
+++ learn-terraform-aws-instance/main.tf
@@ -0,0 +1,56 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0ce4b8a18a8605eff"
+  instance_type = "t2.micro"
+  key_name = "ec2key"
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+
+  tags = {
+    Name = "MartinServer"
+  }
+}
+
+# creating security group with 22 port (SSH) support
+resource "aws_security_group" "main" {
+  egress = [
+    {
+      cidr_blocks      = [ "0.0.0.0/0", ]
+      description      = ""
+      from_port        = 0
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = "-1"
+      security_groups  = []
+      self             = false
+      to_port          = 0
+    }
+  ]
+ ingress                = [
+   {
+     cidr_blocks      = [ "0.0.0.0/0", ]
+     description      = ""
+     from_port        = 22
+     ipv6_cidr_blocks = []
+     prefix_list_ids  = []
+     protocol         = "tcp"
+     security_groups  = []
+     self             = false
+     to_port          = 22
+  }
+  ]
+}
